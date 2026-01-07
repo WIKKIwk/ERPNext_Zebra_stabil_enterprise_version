e5cd014bad8a9e168fa6522d80f318790865a760
WIKKI <uzkingshark@gmail.com>
2026-01-07 22:07:14 +0500

    chore: add src/ZebraBridge.Core/Errors.cs

---
+++ src/ZebraBridge.Core/Errors.cs
@@ -0,0 +1,34 @@
+namespace ZebraBridge.Core;
+
+public class ZebraBridgeException : Exception
+{
+    public ZebraBridgeException(string message) : base(message)
+    {
+    }
+
+    public ZebraBridgeException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
+
+public class InvalidEpcException : ZebraBridgeException
+{
+    public InvalidEpcException(string message) : base(message)
+    {
+    }
+}
+
+public class PrinterNotFoundException : ZebraBridgeException
+{
+    public PrinterNotFoundException(string message) : base(message)
+    {
+    }
+}
+
+public class PrinterCommunicationException : ZebraBridgeException
+{
+    public PrinterCommunicationException(string message, Exception? innerException = null)
+        : base(message, innerException ?? new Exception(message))
+    {
+    }
+}
